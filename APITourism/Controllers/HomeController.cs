a01ac0f92476e64c89b8009f0781da5d8663f1cb
tecnosoftit <tecnosoftit@hotmail.com>
2018-01-31 21:55:03 -0500

    Home change

--- APITourism/Controllers/HomeController.cs
+++ APITourism/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace APITourism.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Pages API";
 
             return View();
         }
