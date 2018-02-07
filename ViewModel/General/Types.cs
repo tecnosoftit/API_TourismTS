6e8f88cc8bd59b1e6e01e1b75762378f5593b2c4
Laura <lmgarcia@tecnosoftit.com>
2018-02-07 09:36:24 -0500

    Revert "Revert "Revert "Commit4"""
    This reverts commit 0a46545855f3aa99897f73a5a10dbf95497dcd84.

--- ViewModel/General/Types.cs
+++ ViewModel/General/Types.cs
@@ -6,16 +6,7 @@ using System.Threading.Tasks;
 
 namespace ViewModel.General
 {
-    public class Types
+    class Types
     {
-        public int IDENTIFICATION { get; set; }
-        public string NAMES { get; set; }
-        public string DESCRIPTIONS { get; set; }
-        public string IMAGE_URL { get; set; }
-        public int TYPE_CLASIFICATION { get; set; }
-        public string CLASIFICATION_NAME { get; set; }
-        public bool STATUS_ { get; set; }
-        public DateTime CREATION { get; set; }
-        public DateTime MODIFICATION { get; set; }
     }
 }
