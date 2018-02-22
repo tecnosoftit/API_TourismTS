a692ee4d07d1ed6dea68d91fecc03dcedf13432c
TecnoSoftit <danielarevalo.1998@gmail.com>
2018-02-22 18:07:33 -0500

    asd

--- ViewModel/General/CreateUser.cs
+++ ViewModel/General/CreateUser.cs
@@ -8,13 +8,12 @@ namespace ViewModel.General
 {
     public class CreateUser
     {
-        public string Username { get; set; }
+        public string Roles { get; set; }
         public string Password { get; set; }
         public string FistName { get; set; }
+        public string SurName { get; set; }
         public string Email { get; set; }
-        public string Company { get; set; }
-
+        public string CompanyId { get; set; }
     }
-
 }
 
